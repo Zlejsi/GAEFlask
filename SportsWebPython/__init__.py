42fd1ccdfcee0cb0920ac8407c9d36d31651b140
Zlejsi <michal.zlacky@seznam.cz>
2018-07-28 20:32:14 +0200

    mysqldb for production

--- SportsWebPython/__init__.py
+++ SportsWebPython/__init__.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #test
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/SportDB'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/SportDB'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:root@localhost/SportDB'
 #production
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@/SportDB?unix_socket=/cloudsql/aerial-chimera-211120:europe-west2:sporteventdb'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.189.122.30/SportDB?unix_socket=/cloudsql/aerial-chimera-211120:europe-west2:sporteventdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:root@35.189.122.30/SportDB?unix_socket=/cloudsql/aerial-chimera-211120:europe-west2:sporteventdb'
 db = SQLAlchemy(app)
 
 #Instance connection name: aerial-chimera-211120:europe-west2:sporteventdb
